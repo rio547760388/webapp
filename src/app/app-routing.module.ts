5bf5e44d9e8cd658a08dd79492af3eb19d96b409
tianjz <jinzhu.tian@onerway.com>
2020-12-03 18:33:00 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroComponent } from './hero/hero.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'heroes', component: HeroComponent},
+  {path: 'hero:id', component: HeroDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
